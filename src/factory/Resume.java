eb0d70dac66714415cd61621e4841eb8e6864732
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-15 12:34:26 +0500

    Update Resume.java

--- src/factory/Resume.java
+++ src/factory/Resume.java
@@ -1,4 +1,8 @@
 package factory;
 
-public class Resume {
+public class Resume implements Document {
+    @Override
+    public void open() {
+        System.out.println("Открыто резюме");
+    }
 }
